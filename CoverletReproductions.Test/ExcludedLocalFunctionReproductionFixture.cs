de36f1f574364af5cf63cb192b95001364a50240
meggima <eggimannm@outlook.com>
2022-02-15 15:17:58 +0100

    add new repro

---
+++ CoverletReproductions.Test/ExcludedLocalFunctionReproductionFixture.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace CoverletReproductions.Test;
+
+public class ExcludedLocalFunctionReproductionFixture
+{
+    [Fact]
+    public void Execute_ShouldWork()
+    {
+        // Arrange
+        var sut = new ExcludedLocalFunctionReproduction();
+
+        // Act
+        sut.SomethingThatIsUsingALocalFunction();
+    }
+}
---
+++ CoverletReproductions/ExcludedLocalFunctionReproduction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CoverletReproductions;
+
+public class ExcludedLocalFunctionReproduction
+{
+    public void SomethingThatIsUsingALocalFunction()
+    {
+        MethodThatTakesALambda(NewMethod());
+
+        [ExcludeFromCodeCoverage(Justification = "Not testable")]
+        static Func<string, bool> NewMethod()
+        {
+            return myString => myString.Length == 10;
+        }
+    }
+
+    private void MethodThatTakesALambda(Func<string, bool> function)
+    {
+        bool _ = function != null;
+    }
+}
