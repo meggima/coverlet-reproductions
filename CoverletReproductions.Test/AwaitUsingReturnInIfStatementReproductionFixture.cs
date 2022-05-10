30b62027b4a57778c07a3edf24bd0d513702b95c
manuelamateo <mail@manuelamateo.ch>
2022-05-10 10:49:05 +0200

    Reproduction return in await using without braces in if statement has uncovered line

---
+++ CoverletReproductions.Test/AwaitUsingReturnInIfStatementReproductionFixture.cs
@@ -0,0 +1,80 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace CoverletReproductions.Test;
+
+public class AwaitUsingReturnInIfStatementReproductionFixture
+{
+    [Fact]
+    public async Task ExecuteReproduction_HasUncoveredLine()
+    {
+        // Arrange
+        var sut = new AwaitUsingReturnInIfStatementReproduction();
+
+        // Act
+        int resultTrue = await sut.ExecuteReproduction(true);
+        int resultFalse = await sut.ExecuteReproduction(false);
+
+        // Assert
+        Assert.Equal(3, resultTrue);
+        Assert.Equal(1, resultFalse);
+    }
+
+    [Fact]
+    public async Task ExecuteNoIfStatement_AllCovered()
+    {
+        // Arrange
+        var sut = new AwaitUsingReturnInIfStatementReproduction();
+
+        // Act
+        int result = await sut.ExecuteNoIfStatement();
+
+        // Assert
+        Assert.Equal(3, result);
+    }
+
+    [Fact]
+    public async Task ExecuteAwaitUsingWithBraces_AllCovered()
+    {
+        // Arrange
+        var sut = new AwaitUsingReturnInIfStatementReproduction();
+
+        // Act
+        int resultTrue = await sut.ExecuteAwaitUsingWithBraces(true);
+        int resultFalse = await sut.ExecuteAwaitUsingWithBraces(false);
+
+        // Assert
+        Assert.Equal(3, resultTrue);
+        Assert.Equal(1, resultFalse);
+    }
+
+    [Fact]
+    public async Task ExecuteUsingWithoutAwait_AllCovered()
+    {
+        // Arrange
+        var sut = new AwaitUsingReturnInIfStatementReproduction();
+
+        // Act
+        int resultTrue = await sut.ExecuteUsingWithoutAwait(true);
+        int resultFalse = await sut.ExecuteUsingWithoutAwait(false);
+
+        // Assert
+        Assert.Equal(3, resultTrue);
+        Assert.Equal(1, resultFalse);
+    }
+
+    [Fact]
+    public async Task ExecuteReturnOutsideIfStatement_AllCovered()
+    {
+        // Arrange
+        var sut = new AwaitUsingReturnInIfStatementReproduction();
+
+        // Act
+        int resultTrue = await sut.ExecuteReturnOutsideIfStatement(true);
+        int resultFalse = await sut.ExecuteReturnOutsideIfStatement(false);
+
+        // Assert
+        Assert.Equal(3, resultTrue);
+        Assert.Equal(1, resultFalse);
+    }
+}
