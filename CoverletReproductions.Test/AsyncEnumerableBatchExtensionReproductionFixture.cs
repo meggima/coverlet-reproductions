54ab0b968af1717cef653fafc69648becd0168e0
manuelamateo <mail@manuelamateo.ch>
2022-05-12 16:31:45 +0200

    Reproduction wong branch coverage in AsyncEnumerable extension

---
+++ CoverletReproductions.Test/AsyncEnumerableBatchExtensionReproductionFixture.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CoverletReproductions.Test;
+
+public class AsyncEnumerableBatchExtensionReproductionFixture
+{
+    [Fact]
+    public async Task ExecuteReproduction_UncoveredBranches()
+    {
+        // Arrange
+        var enumerable = AsyncEnumerable.Range(1, 95);
+
+        // Act
+        IAsyncEnumerable<IAsyncEnumerable<int>> batches = enumerable.ExecuteReproduction(10);
+        await batches.SelectAwait(batch => batch.ToArrayAsync()).ToArrayAsync();
+
+        // Assert
+        // no assert
+    }
+}
---
+++ CoverletReproductions/AsyncEnumerableBatchExtensionReproduction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace CoverletReproductions;
+
+public static class AsyncEnumerableBatchExtensionReproduction
+{
+    public static async IAsyncEnumerable<IAsyncEnumerable<T>> ExecuteReproduction<T>(this IAsyncEnumerable<T> source, int batchSize)
+    {
+        var enumerator = source.GetAsyncEnumerator();
+
+        while (await enumerator.MoveNextAsync())
+        {
+            yield return YieldBatch(enumerator, batchSize);
+        }
+    }
+
+    private static async IAsyncEnumerable<T> YieldBatch<T>(IAsyncEnumerator<T> source, int batchSize)
+    {
+        yield return source.Current;
+
+        for (int i = 1; i < batchSize && await source.MoveNextAsync(); i++)
+        {
+            yield return source.Current;
+        }
+    }
+}
